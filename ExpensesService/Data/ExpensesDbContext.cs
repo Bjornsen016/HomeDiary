14f6c9dbfabdad0a792a942bd2e88f4bed90f9d1
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-12-08 15:16:11 +0100

    Add - dbContext and model

---
+++ ExpensesService/Data/ExpensesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ExpensesService.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesService.Data
+{
+    public class ExpensesDbContext : DbContext
+    {
+        public ExpensesDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Expanse>? Expenses { get; set; }
+    }
+}
